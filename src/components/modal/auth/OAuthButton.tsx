e97c03c3878050924a5c3e8029c4e5dc3fde5b5c
Lietreum <mirsieolien@gmail.com>
2024-05-18 21:03:22 +0700

    p3

--- src/components/modal/auth/OAuthButton.tsx
+++ src/components/modal/auth/OAuthButton.tsx
@@ -1,10 +1,24 @@
 import { Button, Flex, Image, Text } from "@chakra-ui/react";
-import React from "react";
+import { User } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "../../../firebase/clientApp";
+import { auth, firestore } from "../../../firebase/clientApp";
 
-const OAuthButton: React.FC = () => {
-  const [SignInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, userCred, loading, error] =
+    useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, "users", user.uid);
+    await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
 
   return (
     <Flex direction="column" width="100%" mb={4}>
@@ -12,14 +26,14 @@ const OAuthButton: React.FC = () => {
         variant="oauth"
         mb={2}
         isLoading={loading}
-        onClick={() => SignInWithGoogle()}
+        onClick={() => signInWithGoogle()}
       >
-        <Image src="/images/googl.webp" height="38px" mr={1} /> Continue With
-        Google
+        <Image src="/images/googlelogo.png" height="20px" mr={4} />
+        Continue with Google
       </Button>
-      <Button variant="oauth">Others</Button>
+      <Button variant="oauth">Some Other Provider</Button>
       {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
-export default OAuthButton;
+export default OAuthButtons;
--- src/components/modal/auth/SignUp.tsx
+++ src/components/modal/auth/SignUp.tsx
@@ -1,10 +1,12 @@
-import { authModalState } from "@/atoms/authModalAtom";
-import { auth } from "../../../firebase/clientApp";
-import { FIREBASE_ERRORS } from "../../../firebase/errors";
 import { Input, Button, Flex, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
+import { User } from "firebase/auth";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
@@ -14,36 +16,41 @@ const SignUp: React.FC = () => {
     confirmPassword: "",
   });
   const [error, setError] = useState("");
-  const [createUserWithEmailAndPassword, user, loading, userError] =
+  const [createUserWithEmailAndPassword, userCred, loading, userError] =
     useCreateUserWithEmailAndPassword(auth);
-  // Firebase logic
 
+  // Firebase logic
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (error) setError("");
-    if (signUpForm.password.length <= 5) {
-      setError("Password must be at least 6 characters long");
-      return;
-    } else {
-      setError("");
-    }
     if (signUpForm.password !== signUpForm.confirmPassword) {
-      // set error message
       setError("Passwords do not match");
       return;
     }
-
-    // password match check
+    // passwords match
     createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
   };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //update form state
+    // update form state
     setSignUpForm((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
     }));
   };
+
+  const createUserDocument = async (user: User) => {
+    await setDoc(
+      doc(firestore, "users", user.uid),
+      JSON.parse(JSON.stringify(user))
+    );
+  };
+
+  useEffect(() => {
+    if (userCred) {
+      createUserDocument(userCred.user);
+    }
+  }, [userCred]);
 
   return (
     <form onSubmit={onSubmit}>
@@ -67,14 +74,15 @@ const SignUp: React.FC = () => {
           border: "1px solid",
           borderColor: "blue.500",
         }}
+        bg="gray.50"
       />
       <Input
         required
         name="password"
+        onChange={onChange}
         placeholder="password"
         type="password"
         mb={2}
-        onChange={onChange}
         fontSize="10pt"
         _placeholder={{ color: "gray.500" }}
         _hover={{
@@ -88,14 +96,15 @@ const SignUp: React.FC = () => {
           border: "1px solid",
           borderColor: "blue.500",
         }}
+        bg="gray.50"
       />
       <Input
         required
         name="confirmPassword"
-        placeholder="Confirm Password"
+        onChange={onChange}
+        placeholder="confirm password"
         type="password"
         mb={2}
-        onChange={onChange}
         fontSize="10pt"
         _placeholder={{ color: "gray.500" }}
         _hover={{
@@ -109,13 +118,14 @@ const SignUp: React.FC = () => {
           border: "1px solid",
           borderColor: "blue.500",
         }}
+        bg="gray.50"
       />
-        <Text textAlign="center" color="red" fontSize="10pt">
-          {error ||
-            FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
-        </Text>
+      <Text textAlign="center" color="red" fontSize="10pt">
+        {error ||
+          FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
       <Button
-        width="100% "
+        width="100%"
         height="36px"
         mt={2}
         mb={2}
@@ -125,7 +135,7 @@ const SignUp: React.FC = () => {
         Sign Up
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
-        <Text mr={1}>Already a communter?</Text>
+        <Text mr={1}>Already a redditor?</Text>
         <Text
           color="blue.500"
           fontWeight={700}
